684f474ffecf06d89fef14fdc718b782476554ff
Tom <tomasvergara272@gmail.com>
2023-07-15 21:13:02 -0300

    i18n

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  public name: string = 'Tomas';
+  public gender: 'male'|'female' = 'male';
+  
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  public clients:string[] = ['To','Ce','Pau','Wal','Fa','Yo']
+
+  public clientsMap = {
+    '=0': 'no tenemos clientes esperando',
+    '=1': 'tenemos 1 cliente esperando',
+    other: 'tenemos # clientes esperando'
+  }
+
+  changeClient(){
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  deleteClient(){
+    this.clients.shift();
+  }
+
 }
